a7311fec75608c935e0a4339f9175516833fb69e
daitasu <ifctp.dai.monara@gmail.com>
2021-02-09 10:32:29 +0900

    first commit

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+// ライブラリ読み込み
+import express from 'express';
+import router from './routes';
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const port = process.env.PORT || 8000;
+
+app.use('/', router);
+
+// -------------------------------------------------
+//  404
+// -------------------------------------------------
+
+app.use((req, res) => {
+  res.status(404);
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+  res.type('txt').send('Not found');
+});
+
+
+
+app.listen(port);
+console.log('listen on port ' + port);
---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+﻿import express from 'express';
+
+const app = express();
+const router = express.Router();
+
+router.get('/helloWorld', (req, res) => {
+    res.status(200).send({ message: 'Hello, world' });
+});
+
+export default router;
